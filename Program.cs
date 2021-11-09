10c48cc61a54a4b966ccec5d9750f075a2012121
Enduren <dtennison79@gmail.com>
2021-11-08 18:23:27 -0600

    intial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(70);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(21.11);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
